6706ae2018a4234ca06a34f17a8dd0b69280d419
ms-jasondel <jasondel@microsoft.com>
2020-03-29 10:51:48 -0400

    Adding dashboard template

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,13 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
 import theme from './theme';
+import Dashboard from './Dashboard.js'
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
     <CssBaseline />
-    <App />
+    <Dashboard />
   </ThemeProvider>,
   document.querySelector('#root'),
 );
